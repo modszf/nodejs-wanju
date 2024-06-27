0f9bade62cb991b5b5262b72bf691d96653d7920
modszf <botsmods75@gmail.com>
2024-06-27 14:30:56 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,41 +1,21 @@
+
 #!/bin/bash
-## ===========================================设置各参数（不需要的可以删掉或者前面加# ）=============================================
-
-# 设置ARGO参数 (不设置默认使用临时隧道，如果设置把前面的#去掉)
-# export TOK='xxxxx'
-# export ARGO_DOMAIN='xxxxx'
-
-#设置哪吒参数(NEZHA_TLS='1'开启tls,设置其他关闭tls)
-#export NEZHA_SERVER='xxxx'
-#export NEZHA_KEY='xxx'
-#export NEZHA_PORT='443'
-#export NEZHA_TLS='1'
-
-#设置app参数（默认x-ra-y参数，如果你更该了下载地址，需要修改UUID和VPATH）
-export UUID='fd80f56e-93f3-4c85-b2a8-c77216c509a7'
-export VPATH='vls'
-export CF_IP='cdn.xn--b6gac.eu.org'
-
-## ===========================================设置x-ra-y下载地址（建议直接使用默认）===============================
-# 设置amd64-X-A-R-Y下载地址（带内置配置版本）
-export URL_BOT='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-amd-w'
-# 设置arm64_64-X-A-R-Y下载地址（带内置配置版本）
-export URL_BOT2='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-arm-w'
-if command -v curl &>/dev/null; then
-        DOWNLOAD_CMD="curl -sL"
-    # Check if wget is available
-  elif command -v wget &>/dev/null; then
-        DOWNLOAD_CMD="wget -qO-"
-  else
-        echo "Error: Neither curl nor wget found. Please install one of them."
-        sleep 30
-        exit 1
-fi
-arch=$(uname -m)
-if [[ $arch == "x86_64" ]]; then
-$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-amd > /tmp/app
-else
-$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-arm > /tmp/app
-fi
-
-chmod 777 /tmp/app && /tmp/app 
+rm -f web config.json
+wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip temp.zip
+sleep 2
+rm -f temp.zip
+sleep 2
+mv v2ray web
+sleep 2
+rm -rf config.json
+sleep 2
+wget https://raw.githubusercontent.com/PlayBillbes/render-me/main/config.json
+sleep 2
+wget https://github.com/eooce/choreo-2go/raw/main/files/server
+sleep 5
+chmod +x web server
+sleep 5
+nohup ./web run &>/dev/null &
+sleep 5
+nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiNmIwYzRiZDczMjQ4Y2IxNTYyMTdmN2QyNzZlOWE5ZjAiLCJ0IjoiNTkxNjI5YmUtNjI3MS00MGYwLWJjNmItNmIwMTZmYTIxMTE2IiwicyI6Ik56Y3lPVE5pTWpjdE5ERm1NeTAwWldReExXRXhOakV0WVdRMU5qTXpPVEF3WXpneSJ9 >/dev/null 2>&1 &
